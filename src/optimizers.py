5a2ebfe3ba10084a5bfc3cebb624815f7986b58f
김주형 (Kim Juhyeong) <55781022+rlawngud0428@users.noreply.github.com>
2026-05-25 22:18:10 +0900

    feat: network, training 제외 구현 완료 (#3)
    feat: network, training 제외 구현 완료 (#3)

--- src/optimizers.py
+++ src/optimizers.py
@@ -11,14 +11,16 @@ class SGD:
     가장 단순한 optimizer로, 각 파라미터를 gradient 반대 방향으로 lr만큼 이동합니다.
     """
 
-    def __init__(self, lr=0.01):
+    def __init__(self, lr=0.0000000000000000000001):
         """Args: lr: 한 번 업데이트할 때 gradient에 곱할 학습률."""
         self.lr = lr
 
     def update(self, params, grads):
         """params dict의 모든 파라미터를 제자리(in-place)에서 갱신합니다."""
         # TODO: params[key]를 gradient 반대 방향으로 업데이트하세요.
-        raise NotImplementedError("SGD.update를 구현하세요.")
+        # raise NotImplementedError("SGD.update를 구현하세요.")
+        for key in params.keys():
+            params[key] -= self.lr * grads[key]
 
 
 class Adam:
@@ -32,10 +34,26 @@ class Adam:
     def __init__(self, lr=0.001):
         """Args: lr: Adam 업데이트의 기본 학습률."""
         self.lr = lr
-        self.m, self.v = {}, {}
+        self.m = None
+        self.v = None
         self.t = 0
 
     def update(self, params, grads):
         """Adam 공식에 따라 params dict의 모든 파라미터를 갱신합니다."""
         # TODO: m, v 이동평균과 bias correction을 사용해 params를 업데이트하세요.
-        raise NotImplementedError("Adam.update를 구현하세요.")
+        # raise NotImplementedError("Adam.update를 구현하세요.")
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+
+        self.t += 1
+        lr_t = self.lr * np.sqrt(1.0 - (0.999)**self.t) / (1.0 - (0.9)**self.t)
+
+        for key in params.keys():
+            self.m[key] += (1 - 0.9) * (grads[key] - self.m[key])
+            self.v[key] += (1 - 0.999) * (grads[key]**2 - self.m[key])
+
+            params[key] -= lr_t * self.m[key] / np.sqrt(self.v[key] + 1e-7)
+            
