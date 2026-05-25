91092ff74db7566444d34c4613f6f8c83b7c50b2
IMGyuGo <gyugo4894@gmail.com>
2026-05-25 23:58:07 +0900

    feat: network, optimizer, losses
    nn

--- src/losses.py
+++ src/losses.py
@@ -2,6 +2,7 @@
 """손실 함수 모음."""
 
 import numpy as np
+from activations import *
 
 
 def cross_entropy_loss(y_pred, y_true):
@@ -18,3 +19,28 @@ def cross_entropy_loss(y_pred, y_true):
     batch_size = y_pred.shape[0]
     correct_prob = y_pred[np.arange(batch_size), y_true]
     return -np.sum(y_true * np.log(correct_prob)) / batch_size
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None  # 손실함수
+        self.y = None  # softmax의 출력
+        self.t = None  # 정답 레이블(원-핫 인코딩 형태)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = Softmax.forward(x)
+        self.loss = cross_entropy_loss(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size:  # 정답 레이블이 원-핫 인코딩 형태일 때
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+
+        return dx
--- src/optimizers.py
+++ src/optimizers.py
@@ -34,10 +34,25 @@ class Adam:
     def __init__(self, lr=0.001):
         """Args: lr: Adam 업데이트의 기본 학습률."""
         self.lr = lr
-        self.m, self.v = {}, {}
+        self.m = None
+        self.v = None
         self.t = 0
 
     def update(self, params, grads):
         """Adam 공식에 따라 params dict의 모든 파라미터를 갱신합니다."""
         # TODO: m, v 이동평균과 bias correction을 사용해 params를 업데이트하세요.
-        raise NotImplementedError("Adam.update를 구현하세요.")
+        # raise NotImplementedError("Adam.update를 구현하세요.")
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+
+        self.t += 1
+        lr_t = self.lr * np.sqrt(1.0 - (0.999) ** self.t) / (1.0 - (0.9) ** self.t)
+
+        for key in params.keys():
+            self.m[key] += (1 - 0.9) * (grads[key] - self.m[key])
+            self.v[key] += (1 - 0.999) * (grads[key] ** 2 - self.m[key])
+
+            params[key] -= lr_t * self.m[key] / np.sqrt(self.v[key] + 1e-7)
