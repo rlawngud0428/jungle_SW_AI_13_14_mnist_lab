7a000715c135b843917c66beb18d39aebcf388cd
IMGyuGo <gyugo4894@gmail.com>
2026-05-25 20:36:08 +0900

    Affine 구현 완

--- src/layers.py
+++ src/layers.py
@@ -23,6 +23,9 @@ class Affine:
         """가중치 W와 편향 b를 외부 params dict와 같은 배열 객체로 공유합니다."""
         self.W = W
         self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
 
     def forward(self, x):
         """
@@ -33,7 +36,10 @@ class Affine:
             (batch_size, output_dim)
         """
         # TODO: backward에서 사용할 입력 x를 저장하고 x @ W + b를 반환하세요.
-        raise NotImplementedError("Affine.forward를 구현하세요.")
+        # raise NotImplementedError("Affine.forward를 구현하세요.")
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
 
     def backward(self, dout):
         """
@@ -48,7 +54,11 @@ class Affine:
         """
         # TODO: self.dW, self.db, dx를 계산하세요.
         # 힌트: dW = x.T @ dout, db = batch 방향 합, dx = dout @ W.T
-        raise NotImplementedError("Affine.backward를 구현하세요.")
+        # raise NotImplementedError("Affine.backward를 구현하세요.")
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
 
 
 class BatchNorm:
